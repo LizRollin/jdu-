9986a14168a7bff35ee03cc78bbe7ebb828474ef
Alexandre <83257450+alexandregreff@users.noreply.github.com>
2023-11-06 19:33:35 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// server.js (v0.5)
+
 const express = require("express");
 const fs = require("fs");
 var fsPath = require('fs-path');
@@ -5,33 +7,28 @@ const app = express();
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var bodyParser = require('body-parser')
 
-// parse application/x-www-form-urlencoded
+// x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
-// parse application/json
+// json
+
 app.use(bodyParser.json())
-var prodwsurl = "https://jmcs-prod.just-dance.com"
-var room = "MainJD2021"
-var sessions = require("./files/sessions.json");
-var entities = require("./files/entities.json");
-var entitiesphone = require("./files/entities-phone.json");
-var configuration = require("./files/configuration.json");
-var party = require("./files/party.json");
-var upsellvideos = require("./files/upsellvideos.json");
-var skuconstants = require("./files/skuconstants.json");
-var items = require("./files/items.json");
-var blocks = require("./files/blocks.json");
-var mine = require("./files/mine.json");
-var skupackages = require("./files/skupackages.json");
-var profiles = require("./files/profiles.json");
-var quests = require("./files/quests.json");
-var songs = require("./files/songs.json");
-var news = require("./files/news.json");
-var sweat = require("./files/sweat.json");
-var onlinequest = require("./files/onlinequest.json");
-var playlist = require("./files/playlist.json");
-var coop = require("./files/coop.json");
-var bosses = require("./files/bosses.json");
+const prodwsurl = "https://jmcs-prod.just-dance.com"
+
+const entities = require("./files/entities.json");
+const entitiesphone = require("./files/entities-phone.json");
+const configuration = require("./files/configuration.json");
+const party = require("./files/party.json");
+const skuconstants = require("./files/skuconstants.json");
+const mine = require("./files/mine.json");
+const skupackages = require("./files/skupackages.json");
+const songs = require("./files/songs.json");
+
+// party
+
+app.post("/carousel/v2/pages/party", (req, res) => {
+  res.send(party);
+});
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -39,22 +36,7 @@ app.use(function(req, res, next) {
   next();
 });
 
-
-app.post("/carousel/v2/pages/partycoop", (req, res) => {
-  res.send(coop);
-});
-
-app.post("/carousel/v2/pages/quests", (req, res) => {
-  res.send(onlinequest);
-});
-
-app.post("/carousel/v2/pages/create-playlist", (req, res) => {
-  res.send(playlist);
-});
-
-app.post("/carousel/v2/pages/sweat", (req, res) => {
-  res.send(sweat);
-});
+//sessions
 
 app.post('/v3/profiles/sessions', (req, res) => {
     var xhr = new XMLHttpRequest();
@@ -67,6 +49,8 @@ app.post('/v3/profiles/sessions', (req, res) => {
     xhr.send();
     res.send(xhr.responseText);
 });
+
+// entities
 
 app.get(
   "/v2/spaces/f1ae5b84-db7c-481e-9867-861cf1852dc8/entities",
@@ -81,6 +65,8 @@ app.get(
     res.send(entitiesphone);
   }
 );
+
+// configuration
 
 app.get(
   "/v1/applications/341789d4-b41f-4f40-ac79-e2bc4c94ead4/configuration",
@@ -120,19 +106,7 @@ app.post("/subscription/v1/refresh", (req, res) => {
 });
 });
 
-app.post("/carousel/v2/pages/party", (req, res) => {
-  res.send(party);
-});
-
-app.post("/carousel/v2/pages/upsell-videos", (req, res) => {
-  res.send(upsellvideos);
-});
-
 app.get("/com-video/v1/com-videos-fullscreen", (req, res) => {
-  res.send([]);
-});
-
-app.get("/community-remix/v1/active-contest", (req, res) => {
   res.send([]);
 });
 
@@ -140,22 +114,11 @@ app.get("/constant-provider/v1/sku-constants", (req, res) => {
   res.send(skuconstants);
 });
 
-app.get("/customizable-itemdb/v1/items", (req, res) => {
-  res.send(items);
-});
-
-app.get("/dance-machine/v1/blocks", (req, res) => {
-  res.send(blocks);
-});
-
-app.get("/leaderboard/v1/coop_points/mine", (req, res) => {
-  res.send(mine);
-});
-
 app.get("/packages/v1/sku-packages", (req, res) => {
   res.send(skupackages);
 });
 
+// profiles GET and POST
 
 app.get("/profile/v2/profiles", (req, res) => {
   var profileid = req.url.split('=').pop()
@@ -179,9 +142,7 @@ app.post("/profile/v2/profiles", (req, res) => {
   res.send(xhr.responseText);
 });
 
-app.get("/questdb/v1/quests", (req, res) => {
-  res.send(quests);
-});
+// data
 
 app.get("/songdb/v1/songs", (req, res) => {
   res.send(songs);
@@ -193,91 +154,6 @@ app.get("/status/v1/ping", (req, res) => {
 
 app.post("/subscription/v1/refresh", (req, res) => {
   res.send([]);
-});
-
-app.post("/wdf/v1/assign-room", (req, res) => {
-  res.send({
-	"room": room
-});
-});
-//Database
-app.get("/songdb/v1/songs", function(request, response) {
-  const skuId = request.header("X-SkuId");
-  switch (skuId) {
-    case "jd2015-pc-cmos":
-      // Set the variables to SongDB and Carousel
-      var OnlineDB = require("./files/songs.json");
-
-      for (var song in OnlineDB) {
-        var obj = OnlineDB[song];
-        obj.assets["banner_bkgImageUrl"] = obj.assets["expandBkgImageUrl"];
-      }
-      return response.send(OnlineDB);
-    break;
-    case "jd2017-pc-ww":
-      response.send(SongDB);
-    break;
-    default:
-      response.send("Hey there!" + "\n" + "Cosmos's SongDB are currently unavaliable through a browser");
-    break;
-  }
-});
-
-app.get("/wdf/v1/rooms/" + room + "/*", (req, res) => {
-  var ticket = req.header("Authorization")
-  var xhr = new XMLHttpRequest();
-  var n = req.url.lastIndexOf('/');
-  var result = req.url.substr(0)
-  xhr.open('GET', prodwsurl + result, false);
-  xhr.setRequestHeader('X-SkuId', 'jd2017-pc-ww');
-  xhr.setRequestHeader('Authorization', ticket);
-  xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.send();
-  res.send(xhr.responseText);
-});
-
-app.get("/wdf/v1/server-time", (req, res) => {
-  var ticket = req.header("Authorization")
-  var xhr = new XMLHttpRequest();
-  var n = req.url.lastIndexOf('/');
-  var result = req.url.substr(0)
-  xhr.open('GET', prodwsurl + result, false);
-  xhr.setRequestHeader('X-SkuId', 'jd2017-pc-ww');
-  xhr.setRequestHeader('Authorization', ticket);
-  xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.send();
-  res.send(xhr.responseText);
-});
-
-app.get("/wdf/v1/online-bosses", (req, res) => {
-  res.send(bosses);
-});
-
-app.post("/wdf/v1/rooms/" + room + "/*", (req, res) => {
-  var ticket = req.header("Authorization")
-  var xhr = new XMLHttpRequest();
-  var n = req.url.lastIndexOf('/');
-  var result = req.url.substr(0)
-  xhr.open('POST', prodwsurl + result, false);
-  xhr.setRequestHeader('X-SkuId', 'jd2017-pc-ww');
-  xhr.setRequestHeader('Authorization', ticket);
-  xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.send(JSON.stringify(req.body, null, 2));
-  res.send(xhr.responseText);
-});
-
-app.get("/leaderboard/v1/maps/*", (req, res) => {
-  var ticket = req.header("Authorization")
-  var contentlen = req.header("Content-Length")
-  var xhr = new XMLHttpRequest();
-  var n = req.url.lastIndexOf('/');
-  var result = req.url.substr(0)
-  xhr.open('GET', prodwsurl + result, false);
-  xhr.setRequestHeader('X-SkuId', 'jd2017-pc-ww');
-  xhr.setRequestHeader('Authorization', ticket);
-  xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.send();
-  res.send(xhr.responseText);
 });
 
 app.get('/v3/users/*', (req, res) => {
@@ -319,18 +195,6 @@ app.get('/v3/users/*', (req, res) => {
 });
 });
 
-app.delete("/wdf/v1/rooms/" + room + "/session", (req, res) => {
-  var ticket = req.header("Authorization")
-  var contentlen = req.header("Content-Length")
-  var xhr = new XMLHttpRequest();
-  xhr.open('DELETE', 'https://jmcs-prod.just-dance.com/wdf/v1/rooms/' + room + '/session', false);
-  xhr.setRequestHeader('X-SkuId', 'jd2017-pc-ww');
-  xhr.setRequestHeader('Authorization', ticket);
-  xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.send();
-  res.send(xhr.responseText);
-});
-
 app.get('/content-authorization/v1/maps/*', (req, res) => {
   var ss = req.url.split("/").pop();
   const path = './videos/' + ss + '.json'
@@ -369,12 +233,14 @@ function checkHttps(req, res, next) {
 app.all("*", checkHttps);
 app.use(express.static("public"));
 
-// http://expressjs.com/en/starter/basic-routing.html
+// routing
+
 app.get("/", function(request, response) {
   response.sendFile(__dirname + "/views/index.html");
 });
 
 // listen for requests :)
+
 const listener = app.listen(process.env.PORT, function() {
   console.log("Your app is listening on port " + listener.address().port);
 });
