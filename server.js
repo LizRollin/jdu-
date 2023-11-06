3c2df0ea6f2c47269d88cc2c478e1290c493f573
Alexandre <83257450+alexandregreff@users.noreply.github.com>
2023-11-06 20:16:14 -0300

    Update server.js

--- server.js
+++ server.js
@@ -125,6 +125,10 @@ app.get("/com-video/v1/com-videos-fullscreen", (req, res) => {
   res.send([]);
 });
 
+app.get("/community-remix/v1/active-contest", (req, res) => {
+  res.send([]);
+});
+
 app.get("/constant-provider/v1/sku-constants", (req, res) => {
   res.send(skuconstants);
 });
