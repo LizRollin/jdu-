253998b93c6c8c667c4cb72deed987d6b93b7bf8
Alexandre <83257450+alexandregreff@users.noreply.github.com>
2023-11-06 22:33:35 -0300

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ const entitiesphone = require("./files/entities-phone.json");
 const configuration = require("./files/configuration.json");
 const party = require("./files/party.json");
 const skuconstants = require("./files/skuconstants.json");
-const items = require("./files/items.json");
 const blocks = require("./files/blocks.json");
 const mine = require("./files/mine.json");
 const skupackages = require("./files/skupackages.json");
@@ -128,10 +127,6 @@ app.get("/community-remix/v1/active-contest", (req, res) => {
 
 app.get("/constant-provider/v1/sku-constants", (req, res) => {
   res.send(skuconstants);
-});
-
-app.get("/customizable-itemdb/v1/items", (req, res) => {
-  res.send(items);
 });
 
 app.get("/dance-machine/v1/blocks", (req, res) => {
